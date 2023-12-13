5b9a21fd681c1ef7425d287c281b39daa79080cb
Terooo <up202204971@up.pt>
2023-12-13 17:40:44 +0000

    jogo acabado1.0

--- src/main/java/com/aor/hero/viewer/menu/DeadViewer.java
+++ src/main/java/com/aor/hero/viewer/menu/DeadViewer.java
@@ -13,9 +13,10 @@ public class DeadViewer extends Viewer<DeadMenu> {
     @Override
     protected void drawElements(GUI gui) {
         int menuWidth = 100;
-        int entryX = (menuWidth - getModel().getEntry(0).length()) / 2;
+
         for (int i = 0; i < getModel().getNumberEntries(); i++) {
-            gui.drawText(new Position(entryX, 42 + i), getModel().getEntry(i), getModel().isSelected(i) ? "#FF0000" : "#FFFFFF");
+            gui.drawText(new Position(51-8+8*i, 42), getModel().getEntry(i), getModel().isSelected(i) ? "#FF0000" : "#FFFFFF");
         }
+        gui.drawDead(new Position(16,20));
     }
 }
