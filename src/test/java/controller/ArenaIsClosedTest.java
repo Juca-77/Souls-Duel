4ceec628848bb59275bb0cadf46f3703c05b47c3
Terooo <up202204971@up.pt>
2023-12-14 18:40:06 +0000

    fim

--- src/test/java/controller/ArenaIsClosedTest.java
+++ src/test/java/controller/ArenaIsClosedTest.java
@@ -12,6 +12,8 @@ import java.io.IOException;
 import java.util.List;
 
 public class ArenaIsClosedTest {
+
+    //***Erro Stereo??***, o teste roda :)
     @Property
     void allArenasAreClosed(@ForAll @IntRange(min = 1, max = 5) int level,@ForAll @IntRange(min = 49, max = 100) int width, @ForAll @IntRange(min = 60, max = 70) int height, @ForAll List<GUI.@From("moveActions") ACTION> actions) throws IOException {
         ArenaBuilder ab = new ArenaBuilder(level, width, height);
@@ -20,10 +22,10 @@ public class ArenaIsClosedTest {
 
         for (GUI.ACTION action : actions) {
             controller.step(null, action, 100);
-            assert (controller.getModel().getHero().getPosition().getX() > width/2-15);
-            assert (controller.getModel().getHero().getPosition().getY() > height/2-9);
-            assert (controller.getModel().getHero().getPosition().getX() < width/2+15);
-            assert (controller.getModel().getHero().getPosition().getY() < height/2+4);
+            assert (controller.getModel().getHero().getPosition().getX() > ((double)width)/2-15);
+            assert (controller.getModel().getHero().getPosition().getY() > ((double)height)/2-9);
+            assert (controller.getModel().getHero().getPosition().getX() < ((double)width)/2+15);
+            assert (controller.getModel().getHero().getPosition().getY() < ((double)height)/2+4);
         }
     }
 
