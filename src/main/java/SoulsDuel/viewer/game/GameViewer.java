331f57055652892fc3e6bec0da37da7410266a63
Dark System <rey.joao@hotmail.com>
2023-12-16 00:47:39 +0000

    Fixed all the possible crashes

--- src/main/java/SoulsDuel/viewer/game/GameViewer.java
+++ src/main/java/SoulsDuel/viewer/game/GameViewer.java
@@ -6,6 +6,7 @@ import SoulsDuel.model.game.arena.Arena;
 import SoulsDuel.model.game.elements.Element;
 import SoulsDuel.viewer.Viewer;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class GameViewer extends Viewer<Arena> {
@@ -13,17 +14,16 @@ public class GameViewer extends Viewer<Arena> {
         super(arena);
     }
 
-    @Override
     public void drawElements(GUI gui) {
-        drawElements(gui, getModel().getWalls(), new WallViewer());
-        //drawElements(gui, getModel().getMonsters(), new MonsterViewer());
+        drawElements(gui, new ArrayList<>(getModel().getWalls()), new WallViewer());
+        // drawElements(gui, new ArrayList<>(getModel().getMonsters()), new MonsterViewer());
         drawElement(gui, getModel().getHero(), new SoulViewer());
         drawElement(gui, getModel().getEnemy(), new EnemyViewer());
-        drawElements(gui, getModel().getBlades(), new BladeViewer());
-        drawElements(gui, getModel().getGrenades(), new GrenadeViewer());
+        drawElements(gui, new ArrayList<>(getModel().getBlades()), new BladeViewer());
+        drawElements(gui, new ArrayList<>(getModel().getGrenades()), new GrenadeViewer());
 
-        gui.drawText(new Position(getModel().getWidth()/2-15, 41), "HP: " + getModel().getHero().getHp() + "/30", "#FFFFFF");
-        gui.drawText(new Position(getModel().getWidth()/2+9, 41), "LEVEL:" + getModel().getLevel(), "#FFFFFF");
+        gui.drawText(new Position(getModel().getWidth() / 2 - 15, 41), "HP: " + getModel().getHero().getHp() + "/30", "#FFFFFF");
+        gui.drawText(new Position(getModel().getWidth() / 2 + 9, 41), "LEVEL:" + getModel().getLevel(), "#FFFFFF");
     }
 
     private <T extends Element> void drawElements(GUI gui, List<T> elements, ElementViewer<T> viewer) {
@@ -35,3 +35,6 @@ public class GameViewer extends Viewer<Arena> {
         viewer.draw(element, gui);
     }
 }
+
+
+
